d19b14454f4520292dcfe6c72e1bdd0d339448f9
Ubuntu <ubuntu@ip-172-31-11-75.ec2.internal>
2023-03-22 16:44:43 +0000

    created login page

--- WebApp/backend.py
+++ WebApp/backend.py
@@ -1,14 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+import mysql.connector
+from mysql.connector import Error
 
 app = Flask(__name__)
+
+def create_db_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("MyNigel-SQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+    return connection
 
 @app.route("/")
 def home():
     return render_template("home.html")
     
-@app.route("/about")
-def about():
-    return render_template("about.html")
-    
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
 if __name__ == "__main__":
+    connection = create_db_connection("44.200.118.80", "ece482", "ece482db", "EC2Test")
     app.run(host='0.0.0.0')
